b6bc2a82d2bdcac755f3b06b50149a58d1345b08
marie <dvorzak.marie@gmx.at>
2019-09-04 22:27:22 +0200

    resize videos

--- functions.php
+++ functions.php
@@ -22,3 +22,10 @@ function the_category_unlinked($separator = ' ') {
     echo $thelist;
 }
 ?>
+
+function alx_embed_html( $html ) {
+    return '<div class="video-container">' . $html . '</div>';
+}
+ 
+add_filter( 'embed_oembed_html', 'alx_embed_html', 10, 3 );
+add_filter( 'video_embed_html', 'alx_embed_html' );
