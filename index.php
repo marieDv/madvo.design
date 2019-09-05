942bc37ebd25b1401159d2b277bd49fa02108145
marie <dvorzak.marie@gmx.at>
2019-09-05 15:12:17 +0200

    minor fixes

--- index.php
+++ index.php
@@ -79,7 +79,7 @@
         <?php
         global $i;
         global $post;
-        $args = array('category' => 25, 'posts_per_page' => -1,); ///25 //2 for dev
+        $args = array('category' => 2, 'posts_per_page' => -1,); ///25 //2 for dev
         $work = get_posts($args);
 
         $args2 = array('category' => 30); ///
@@ -161,7 +161,7 @@
             <?php
             global $i;
             global $post;
-            $args = array('category' => 25); //25 //2 for dev
+            $args = array('category' => 2); //25 //2 for dev
             $work = get_posts($args);
 
             $argse2 = array('category' => 30);
